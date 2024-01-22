b3eeca3f3610841daecc0dcca46a3ea2ca14805b
tom8531400 <134759040+tom8531400@users.noreply.github.com>
2024-01-22 22:28:37 +0800

    商品功能-Refactor category 使用Enum類型

---
+++ spring-mall/src/main/java/com/lee/springmall/constant/ProductCategory.java
@@ -0,0 +1,8 @@
+package com.lee.springmall.constant;
+
+public enum ProductCategory  {
+    FOOD,
+    CAR,
+    BOOK,
+    E_BOOK
+}
--- spring-mall/src/main/java/com/lee/springmall/vo/ProductVo.java
+++ spring-mall/src/main/java/com/lee/springmall/vo/ProductVo.java
@@ -1,31 +1,34 @@
 package com.lee.springmall.vo;
 
+import com.lee.springmall.constant.ProductCategory;
 import lombok.Data;
 
 import java.util.Date;
+
 @Data
 public class ProductVo {
     private Integer product_id;
     private String product_name;
-    private String category;
+    // 商品分類
+    private ProductCategory category;
     private String image_url;
     private Integer price;
     private Integer stock;
     private String description;
-    private Date createdDate;
-    private Date lastModifiedDate;
+    private Date created_date;
+    private Date lastModified_date;
 
     public ProductVo() {
     }
 
-    public ProductVo(String product_name, String category, String image_url, Integer price, Integer stock, String description, Date createdDate, Date lastModifiedDate) {
+    public ProductVo(String product_name, ProductCategory category, String image_url, Integer price, Integer stock, String description, Date created_date, Date lastModified_date) {
         this.product_name = product_name;
         this.category = category;
         this.image_url = image_url;
         this.price = price;
         this.stock = stock;
         this.description = description;
-        this.createdDate = createdDate;
-        this.lastModifiedDate = lastModifiedDate;
+        this.created_date = created_date;
+        this.lastModified_date = lastModified_date;
     }
 }
