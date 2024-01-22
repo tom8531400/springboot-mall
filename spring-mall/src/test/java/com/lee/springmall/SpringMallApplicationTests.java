6d642056987f4cc953bc2c609b7becafdbfd15fc
tom8531400 <134759040+tom8531400@users.noreply.github.com>
2024-01-22 15:51:11 +0800

    新開專案

---
+++ spring-mall/src/test/java/com/lee/springmall/SpringMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.springmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
